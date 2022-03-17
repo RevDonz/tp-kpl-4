6af019307c74b323063185dfc06fced4a003825e
RevDonz <aprilio842@gmail.com>
2022-03-17 19:07:46 +0700

    first init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace tpmodul4_1302204051;
+
+public class ClassMain
+{
+    public static void Main(string[] args)
+    {
+        String nama = "Reva Doni Aprilio";
+        HaloGeneric.SapaUser<string>(ref nama);
+    }
+}
