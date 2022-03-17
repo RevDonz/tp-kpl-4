fb0d1571a40a02e9c1dc54a64d833f0a5506be43
RevDonz <aprilio842@gmail.com>
2022-03-17 19:28:50 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using static tpmodul4_1302204051.ClassMain;
+
 namespace tpmodul4_1302204051;
 
 public class ClassMain
 {
     public static void Main(string[] args)
     {
-        String nama = "Reva Doni Aprilio";
-        HaloGeneric.SapaUser<string>(ref nama);
+        DataGeneric<string> dataGeneric = new DataGeneric<string>("1302204051");
+        dataGeneric.PrintData();
+    }
+
+    
+}
+public class DataGeneric<T>
+{
+    private T Data;
+
+    public DataGeneric(T Data)
+    {
+        this.Data = Data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah " + Data);
     }
 }
